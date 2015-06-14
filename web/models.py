b0dcd2cd461c4db5df01b8abb1d2401dd480b84e
Michael Herman <hermanmu@gmail.com>
2015-06-14 15:20:12 -0600

    Update models.py

--- web/models.py
+++ web/models.py
@@ -7,7 +7,7 @@ from app import db
 
 class Post(db.Model):
 
-    __tablename__ = "posts"
+    __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
